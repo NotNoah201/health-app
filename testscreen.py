0fec808bff78e9fa27aee5b2f8f66eec8177e52f
CallumStone <callumstone59@gmail.com>
2024-06-15 16:05:06 +0100

    importing

--- testscreen.py
+++ testscreen.py
@@ -1,5 +1,5 @@
 #this is for me and jake. test screen.
-from PyQt5 import *
-from QWidget, Q import*
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout
 
 print('test')
