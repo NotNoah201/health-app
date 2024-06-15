08ddd261cca9c508cbf8db8daf68ae245f6ca748
Notnoah <are.noah700@gmail.com>
2024-06-15 16:26:07 +0100

    sigma

--- Results.py
+++ Results.py
@@ -1,3 +1,11 @@
-from demo import demonstration
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLabel, QHBoxLayout, QRadioButton
+from testscreen import *
 
-print(demonstration)
+app = QApplication([])
+main_window = QWidget()
+main_window.show()
+
+main_window.setWindowTitle("Instructions")
+main_window.resize(1920, 1080)
+main_window.move(0,0)
