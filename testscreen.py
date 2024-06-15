3f33d9b79a3868ca0539351662cc004bd67db3af
CallumStone <callumstone59@gmail.com>
2024-06-15 16:21:41 +0100

    this

--- testscreen.py
+++ testscreen.py
@@ -2,4 +2,14 @@
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout
 
+app = QApplication
+
+
+main_win = QWidget()
+main_win.resize(1000, 1000)
+
+
+
+
+
 print('test')
