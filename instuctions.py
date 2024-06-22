12e3ba8d1e7df9adf8de706311c81d694946d099
MannyD94 <dennismanny99@gmail.com>
2024-06-22 15:05:06 +0100

    s

--- instuctions.py
+++ instuctions.py
@@ -1,16 +1,6 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLabel, QHBoxLayout, QRadioButton
 
-one =("This application allows you to use the Rufier test to make an initial diagnosis of your health.")
-two = ("The Rufier test is a set of physical exercises designed to assess your cardiac performance during physical exertion.") 
-three = ("The subject lies in the supine position for 5 minutes and has their pulse rate measured for 15 seconds;")
-four = ("then, within 45 seconds, the subject performs 30 squats.")
-five = ("When the exercise ends, the subject lies down and their pulse is measured again for the first 15 seconds")
-six = ("and then for the last 15 seconds of the first minute of the recovery period.")
-seven = ("Important! If you feel unwell during the test (dizziness,")
-eight = ("tinnitus, shortness of breath, etc.), stop the test and consult a physician.")
-
-
 
 app = QApplication([])
 main_window = QWidget()
@@ -37,4 +27,5 @@ vertical.addWidget(button, alignment = Qt.AlignCenter)
 main_window.setLayout(vertical)
 main_window.setLayout(horizontal)
 
+button.clicked.connect(show_winner)
 app.exec_()
