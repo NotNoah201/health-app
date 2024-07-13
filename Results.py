7d1ddff31062e3100aefc54f1d2d349759ceebec
Notnoah <are.noah700@gmail.com>
2024-07-13 16:00:31 +0100

    !

--- Results.py
+++ Results.py
@@ -12,11 +12,11 @@ class PageThree(QWidget):
         self.initUI()
 
     def initUI(self):
-        self.setWindowTitle("THE RESULTS")
+        self.setWindowTitle("RESULTS")
         self.resize(1920, 1080)
         self.move(0, 0)
 
-        text1 = QLabel('RESULTS:', self)
+        text1 = QLabel('THE RESULTS:', self)
         text1.setAlignment(Qt.AlignCenter)
 
         ruffier_score = evaluate_expression(self.p1, self.p2, self.p3)
@@ -41,25 +41,25 @@ def evaluate_expression(p1, p2, p3):
 ScoreInterpret = {(15,100): {(15,100): "Low",
                              (11,14.9): "Satisfactory",
                              (6,10.9): "Average",
-                             (0.5,5.9): "Above average",
+                             (0.5,5.9): "Above Average",
                              (0,0.4): "High"},
                     (13,14) : {(16.5,100): "Low",
-                               (12.5,16.4): "satisfactory",
+                               (12.5,16.4): "Satisfactory",
                                (7.5,12.4): "Average",
                                (2,7.4): "Above Average",
                                (0,1.9): "High"},
                     (11,12) : {(18,100): "Low",
-                               (14,17.9): "satisfactory",
+                               (14,17.9): "Satisfactory",
                                (9,13.9): "Average",
                                (3.5,8.9): "Above Average",
                                (0,3.4): "High"},
                     (9,10) : {(19.5,100): "Low",
-                               (15.5,19.4): "satisfactory",
+                               (15.5,19.4): "Satisfactory",
                                (10.5,15.4): "Average",
                                (5,10.4): "Above Average",
                                (0,4.9): "High"},
                      (7,8) : {(21,100): "Low",
-                               (17,20.9): "satisfactory",
+                               (17,20.9): "Satisfactory",
                                (12,16.9): "Average",
                                (6.5,11.9): "Above Average",
                                (0,6.4): "High"},
